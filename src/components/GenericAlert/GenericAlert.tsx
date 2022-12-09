cca0cdf5e97ac6a27c8029607ced7e442becd23c
caposcar1998 <oscarcontrerascap@gmail.com>
2022-12-09 00:00:55 -0600

    alert working

--- src/components/GenericAlert/GenericAlert.tsx
+++ src/components/GenericAlert/GenericAlert.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent, useEffect, useState } from "react";
+import { FunctionComponent, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Alert from "@mui/material/Alert";
 
 type GenericAlertType = {
@@ -12,13 +13,19 @@ const GenericAlert: FunctionComponent<GenericAlertType> = ({
   message,
   setAlertVisibility,
 }) => {
+  const navigate = useNavigate();
   const alertType = typeAlert || "info";
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setAlertVisibility(false);
     }, 2000);
-    return () => clearTimeout(timer);
+    return () => {
+      clearTimeout(timer);
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+    };
   }, []);
 
   return <Alert severity={alertType}>{message}</Alert>;
