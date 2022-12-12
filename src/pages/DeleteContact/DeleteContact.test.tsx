1b9e9b5829e345d3c9eddb88881819c1ac05eea8
caposcar1998 <oscarcontrerascap@gmail.com>
2022-12-11 23:12:51 -0600

    more tests

--- src/pages/DeleteContact/DeleteContact.test.tsx
+++ src/pages/DeleteContact/DeleteContact.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import DeleteContact from "./DeleteContact";
 
-test("tests create contact", () => {
+test("tests delete is in the screen", () => {
   render(<DeleteContact />);
-  const linkElement = screen.getByText(/Nombre/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonDelete = screen.getByText(/Si/i);
+  expect(buttonDelete).toBeInTheDocument();
 });
